4097c3c16e11b8ccc49f16cabfec0bb49319afc9
Shane Moyo <smoyo@pdx.edu>
2017-09-17 09:45:55 -0700

    query github api

--- public/scripts/pageCont.js
+++ public/scripts/pageCont.js
@@ -1,15 +1,21 @@
 page('/', myHome);
 page('/About123', myAbout);
 page();
-
+var repos = [];
 function myHome(){
   console.log('going home!');
   $('#information').hide();
   $('article').show();
 }
 
-function myAbout(){
+function myAbout() {
   console.log('going to about!');
   $('article').hide();
   $('#information').show();
+  $.get({
+    url: `github/user/repos`,
+    type: 'GET'
+  })
+    .then(data => repos = data, err => console.error(err)) 
+    .then(console.log(repos));
 }
--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 
+require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
+const requestProxy = require('express-request-proxy');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use( express.static('./public'));
 app.get('/', function( request, response ){
   response.sendFile('index.html', { root: '/public'});
 });
 
+function proxyGitHub( request, response ) {
+  console.log( 'Routing GitHub request for', request.params[0] );
+  ( requestProxy({
+    url: `https://api.github.com/${request.params[0]}`,
+    headers: {Authorization: `token ${process.env.GITHUB_TOKEN}`}
+  }))(request, response);
+}
+
+app.get('/github/*', proxyGitHub);
 app.listen(PORT, function(){
 })
 
